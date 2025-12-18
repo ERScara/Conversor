a3944e139ea008eec2c9b749c8295a9157ad79bf
ERScara <esteban.scaramuzza@gmail.com>
2025-12-18 16:32:24 -0300

    New Features
    # Added temperature boundaries to take the Absolute Zero into account.

--- Conversor.cpp
+++ Conversor.cpp
@@ -1,11 +1,25 @@
 #include "Conversor.h"
+#using <System.Windows.Forms.dll>
+
+using namespace System::Windows::Forms;
 
 namespace Conversor
 {
 	double TempConvert::ConvertF2C(double dFahrenheit) {
-		return ((dFahrenheit - 32.0) * (5.0 / 9.0));
+		if (dFahrenheit < -459.67) {
+			MessageBox::Show("Values below -459.67ºF are not meaningful since it's the absolute zero temperature!", "Below Absolute Zero", MessageBoxButtons::OK, MessageBoxIcon::Warning);
+			return 0.0;
+		}
+		else {
+			return ((dFahrenheit - 32.0) * (5.0 / 9.0));
+		}
 	}
 	double TempConvert::ConvertC2F(double dCelsius) {
+		if (dCelsius < -273.15) {
+			MessageBox::Show("Values below -273.15ºC are not meaningful since it's the absolute zero temperature!", "Below Absolute Zero", MessageBoxButtons::OK, MessageBoxIcon::Warning);
+			return 0.0;
+		} else {
 		return (9.0 / 5.0 * dCelsius) + 32;
+		}
 	}
 }
